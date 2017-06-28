93cb22056ba01046dc53f842917215a83a15afb1
Ivan Kochurkin <kvanttt@gmail.com>
2017-06-28 17:54:47 +0300

    Added Utils for tests.

---
+++ HabraMark.Tests/Utils.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using System.Runtime.CompilerServices;
+
+namespace HabraMark.Tests
+{
+    public static class Utils
+    {
+        public static string ProjectDir { get; private set; }
+
+        static Utils()
+        {
+            InitProjectDir();
+        }
+        
+        public static string ReadFileFromProject(string fileName)
+        {
+            return File.ReadAllText(Path.Combine(ProjectDir, fileName));
+        }
+
+        private static void InitProjectDir([CallerFilePath]string thisFilePath = null)
+        {
+            ProjectDir = Path.GetDirectoryName(thisFilePath);
+        }
+    }
+}
