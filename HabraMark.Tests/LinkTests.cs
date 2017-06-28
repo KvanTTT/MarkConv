397331eb34b4f7cc9432c6bfa9ec6a02da7af0be
Ivan Kochurkin <kvanttt@gmail.com>
2017-06-28 17:56:55 +0300

    Added LinkTests (relative links in VisualCode, GitHub and Habrahabr).
    Added RelativeLinks.*.md files for LinkTests.

---
+++ HabraMark.Tests/LinkTests.cs
@@ -0,0 +1,180 @@
+﻿using NUnit.Framework;
+
+namespace HabraMark.Tests
+{
+    [TestFixture]
+    public class LinkTests
+    {
+        [Test]
+        public void ConvertVisualCodeToGitHubRelativeLinks()
+        {
+            string expected =
+                "* [header-2](#header-2)\n" +
+                "    * [header-3](#header-3)\n" +
+                "* [заголовок-2](#Заголовок-2)\n" +
+                "    * [заголовок-3](#Заголовок-3)\n" +
+                "    * [заголовок-header-3](#Заголовок-header-3)\n" +
+                "* [missing-отсутствует](#missing-отсутствует)\n" +
+                "\n" +
+                "## Header 2\n" +
+                "### Header 3\n" +
+                "## Заголовок 2\n" +
+                "### Заголовок 3\n" +
+                "### Заголовок Header 3";
+
+            Compare("RelativeLinks.VisualCode.md", expected, RelativeLinksKind.VisualCode, RelativeLinksKind.GitHub);
+        }
+
+        [Test]
+        public void ConvertVisualCodeToHabrahabrRelativeLinks()
+        {
+            string expected =
+                "* [header-2](#header-2)\n" +
+                "    * [header-3](#header-3)\n" +
+                "* [заголовок-2](#zagolovok-2)\n" +
+                "    * [заголовок-3](#zagolovok-3)\n" +
+                "    * [заголовок-header-3](#zagolovok-header-3)\n" +
+                "* [missing-отсутствует](#missing-otsutstvuet)\n" +
+                "\n" +
+                "## Header 2\n" +
+                "### Header 3\n" +
+                "## Заголовок 2\n" +
+                "### Заголовок 3\n" +
+                "### Заголовок Header 3";
+
+            Compare("RelativeLinks.VisualCode.md", expected, RelativeLinksKind.VisualCode, RelativeLinksKind.Habrahabr);
+        }
+
+        [Test]
+        public void ConvertGitHubToHabrahabrRelativeLinks()
+        {
+            string expected =
+                "* [header-2](#header-2)\n" +
+                "    * [header-3](#header-3)\n" +
+                "    * [header-3-1](#header-3-1)\n" +
+                "    * [header-3-1-1](#header-3-1-1)\n" +
+                "* [Заголовок-2](#zagolovok-2)\n" +
+                "    * [Заголовок-3](#zagolovok-3)\n" +
+                "    * [Заголовок-3-1](#zagolovok-3-1)\n" +
+                "    * [Заголовок-3-1-1](#zagolovok-3-1-1)\n" +
+                "    * [ЗАГОЛОВОК-3](#zagolovok-3-2)\n" +
+                "    * [Заголовок-header-3](#zagolovok-header-3)\n" +
+                "* [Missing-Пропущенный](#missing-propuschennyy)\n" +
+                "\n" +
+                "## Header 2\n" +
+                "### Header 3\n" +
+                "### Header 3\n" +
+                "### Header 3 1\n" +
+                "\n" +
+                "## Заголовок 2\n" +
+                "### Заголовок 3\n" +
+                "### Заголовок 3\n" +
+                "### Заголовок 3 1\n" +
+                "### ЗАГОЛОВОК 3\n" +
+                "### Заголовок Header 3";
+
+            Compare("RelativeLinks.GitHub.md", expected, RelativeLinksKind.GitHub, RelativeLinksKind.Habrahabr);
+        }
+
+        [Test]
+        public void ConvertHabrahabrToGitHubRelativeLinks()
+        {
+            string expected =
+                "* [header-2](#header-2)\n" +
+                "    * [header-3](#header-3)\n" +
+                "    * [header-3-1](#header-3-1)\n" +
+                "* [zagolovok-2](#Заголовок-2)\n" +
+                "    * [zagolovok-3](#Заголовок-3)\n" +
+                "    * [zagolovok-3-1](#ЗАГОЛОВОК-3-1)\n" +
+                "    * [zagolovok-header-3](#Заголовок-header-3)\n" +
+                "* [missing-propuschennyy](#missing-propuschennyy)\n" +
+                "\n" +
+                "## Header 2\n" +
+                "### Header 3\n" +
+                "### Header 3\n" +
+                "## Заголовок 2\n" +
+                "### Заголовок 3\n" +
+                "### ЗАГОЛОВОК 3 1\n" +
+                "### Заголовок Header 3";
+
+            Compare("RelativeLinks.Habrahabr.md", expected, RelativeLinksKind.Habrahabr, RelativeLinksKind.GitHub);
+        }
+
+        [Test]
+        public void GenerateHabrahabrLink()
+        {
+            string header = @"АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ  ABCabc    0123456789!""№;%:?*() -+=`~<>@#$^&[]{}\/|'_";
+            string habraLink = Header.HeaderToHabralink(header);
+            Assert.AreEqual(@"abvgdeyozhziyklmnoprstufhcchshschyeyuya--abcabc----0123456789--_", habraLink);
+        }
+
+        [Test]
+        public void GenerateVisualCodeLink()
+        {
+            string header = @"ABCabc АБВгде    0123456789!""№;%:?*() -+=`~<>@#$^&[]{}\/|'_";
+            string resultLink = Header.HeaderToLink(header, true);
+            Assert.AreEqual(@"abcabc-абвгде----0123456789--_", resultLink);
+        }
+
+        [Test]
+        public void GenerateGitHubLink()
+        {
+            string header = @"ABCabc АБВгде    0123456789!""№;%:?*() -+=`~<>@#$^&[]{}\/|'_";
+            string resultLink = Header.HeaderToLink(header, false);
+            Assert.AreEqual(@"abcabc-АБВгде----0123456789--_", resultLink);
+        }
+
+        [Test]
+        public void ShouldAddHeaderImageLink()
+        {
+            var options = new ProcessorOptions { HeaderImageLink = "https://github.com/KvanTTT/HabraMark" };
+            var processor = new Processor(options);
+            string actual = processor.Process(
+                "# Header\n" +
+                "\n" +
+                "Paragraph [Some link](https://google.com)\n" +
+                "\n" +
+                "![Header Image](https://hsto.org/storage3/20b/eb7/170/20beb7170a61ec7cf9f4c02f8271f49c.jpg)");
+
+            Assert.AreEqual("# Header\n" +
+                "\n" +
+                "Paragraph [Some link](https://google.com)\n" +
+                "\n" +
+                "[![Header Image](https://hsto.org/storage3/20b/eb7/170/20beb7170a61ec7cf9f4c02f8271f49c.jpg)](https://github.com/KvanTTT/HabraMark)",
+                actual);
+        }
+
+        [Test]
+        public void ShouldRemoveFirstLevelHeader()
+        {
+            var options = new ProcessorOptions { RemoveTitleHeader = true };
+            var processor = new Processor(options);
+            string actual = processor.Process(
+                "# Header\n" +
+                "\n" +
+                "Paragraph text\n" +
+                "\n" +
+                "## Header 2");
+
+            Assert.AreEqual(
+                "Paragraph text\n" +
+                "\n" +
+                "## Header 2", actual);
+        }
+
+        private void Compare(string inputFileName, string outputResult, RelativeLinksKind inputKind, RelativeLinksKind outputKind)
+        {
+            var options = new ProcessorOptions
+            {
+                InputRelativeLinksKind = inputKind,
+                OutputRelativeLinksKind = outputKind
+            };
+
+            var processor = new Processor(options);
+            string source = Utils.ReadFileFromProject(inputFileName);
+            string actual = processor.Process(source);
+
+            Assert.AreEqual(outputResult, actual);
+        }
+    }
+}
---
+++ HabraMark.Tests/RelativeLinks.Habrahabr.md
@@ -0,0 +1,16 @@
+* [header-2](#header-2)
+    * [header-3](#header-3)
+    * [header-3-1](#header-3-1)
+* [zagolovok-2](#zagolovok-2)
+    * [zagolovok-3](#zagolovok-3)
+    * [zagolovok-3-1](#zagolovok-3-1)
+    * [zagolovok-header-3](#zagolovok-header-3)
+* [missing-propuschennyy](#missing-propuschennyy)
+
+## Header 2
+### Header 3
+### Header 3
+## Заголовок 2
+### Заголовок 3
+### ЗАГОЛОВОК 3 1
+### Заголовок Header 3
