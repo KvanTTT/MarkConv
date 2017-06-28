bf417e6b0f4f2e12e2542dea2f27bd612b010a44
Ivan Kochurkin <kvanttt@gmail.com>
2017-06-28 17:48:10 +0300

    Added ProcessorOptions.

---
+++ HabraMark/ProcessorOptions.cs
@@ -0,0 +1,28 @@
+﻿namespace HabraMark
+{
+    public class ProcessorOptions
+    {
+        /// <summary>
+        /// 0 - not change
+        /// -1 - concat lines
+        /// </summary>
+        public int LinesMaxLength { get; set; } = -1;
+
+        public bool RemoveTitleHeader { get; set; } = false;
+
+        public RelativeLinksKind InputRelativeLinksKind { get; set; } = RelativeLinksKind.Default;
+
+        public RelativeLinksKind OutputRelativeLinksKind { get; set; } = RelativeLinksKind.Default;
+
+        public string HeaderImageLink { get; set; } = string.Empty;
+
+        public bool ReplaceSpoilers { get; set; } = true;
+
+        public bool RemoveUnwantedBreaks { get; set; } = true;
+
+        public static ProcessorOptions FromOptions(ProcessorOptions options)
+        {
+            return (ProcessorOptions)options.MemberwiseClone();
+        }
+    }
+}
