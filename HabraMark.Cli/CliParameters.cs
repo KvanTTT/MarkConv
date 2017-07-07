ca385d33a2a81c790cc2d5470072c1148e73585c
Ivan Kochurkin <kvanttt@gmail.com>
2017-07-07 18:23:24 +0300

    HabraMark.Cli: use CommandLineParser dependency for parsing command line parameters.
    Added CliParameters.

---
+++ HabraMark.Cli/CliParameters.cs
@@ -0,0 +1,31 @@
+﻿using CommandLine;
+
+namespace HabraMark.Cli
+{
+    public class CliParameters
+    {
+        [Option('f', "file", Required = true, HelpText = "Input file to be processed")]
+        public string InputFileName { get; set; }
+
+        [Option('i', "inputType")]
+        public MarkdownType InputMarkdownType { get; set; } = MarkdownType.Default;
+
+        [Option('o', "outputType")]
+        public MarkdownType OutputMarkdownType { get; set; } = MarkdownType.Default;
+
+        [Option('l', "linesLength", HelpText = "Lines max length. 0 - not change, -1 - merge lines")]
+        public int? LinesMaxLength { get; set; } = null;
+
+        [Option]
+        public string HeaderImageLink { get; set; } = null;
+
+        [Option]
+        public bool? RemoveTitleHeader { get; set; } = null;
+
+        [Option]
+        public bool? RemoveUnwantedBreaks { get; set; } = null;
+
+        [Option]
+        public bool Normalize { get; set; } = false;
+    }
+}
--- HabraMark.Cli/Program.cs
+++ HabraMark.Cli/Program.cs
@@ -1,31 +1,46 @@
-﻿using System.IO;
+﻿using CommandLine;
+using System.IO;
 
 namespace HabraMark.Cli
 {
     class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
-            string directory = Path.GetDirectoryName(args[0]);
-            string fileName = Path.GetFileNameWithoutExtension(args[0]);
+            ParserResult<CliParameters> parserResult = Parser.Default.ParseArguments<CliParameters>(args);
 
-            var data = File.ReadAllText(args[0]);
-            var options = new ProcessorOptions
-            {
-                LinesMaxLength = -1,
-                RemoveTitleHeader = true,
-                InputMarkdownType = MarkdownType.Default,
-                OutputMarkdownType = MarkdownType.Habrahabr,
-                HeaderImageLink = "",
-                RemoveUnwantedBreaks = true
-            };
-            var processor = new Processor(options)
-            {
-                Logger = new ConsoleLogger()
-            };
-            var converted = processor.Process(data);
+            return parserResult.MapResult(
+                cliParams => Convert(cliParams),
+                errs => 1);
+        }
 
-            File.WriteAllText(Path.Combine(directory, $"{fileName}_habr.md"), converted);
+        private static int Convert(CliParameters parameters)
+        {
+            string directory = Path.GetDirectoryName(parameters.InputFileName);
+            string fileName = Path.GetFileNameWithoutExtension(parameters.InputFileName);
+
+            var data = File.ReadAllText(parameters.InputFileName);
+            var options = ProcessorOptions.GetDefaultOptions(parameters.InputMarkdownType, parameters.OutputMarkdownType);
+
+            if (parameters.LinesMaxLength.HasValue)
+                options.LinesMaxLength = parameters.LinesMaxLength.Value;
+
+            if (parameters.HeaderImageLink != null)
+                options.HeaderImageLink = parameters.HeaderImageLink;
+
+            if (parameters.RemoveTitleHeader.HasValue)
+                options.RemoveTitleHeader = parameters.RemoveTitleHeader.Value;
+
+            if (parameters.RemoveUnwantedBreaks.HasValue)
+                options.RemoveUnwantedBreaks = parameters.RemoveUnwantedBreaks.Value;
+
+            var processor = new Processor(options) { Logger = new ConsoleLogger() };
+            var converted = processor.ProcessAndGetTableOfContents(data);
+            //string tableOfContents = string.Join("\n", converted.TableOfContents);
+
+            File.WriteAllText(Path.Combine(directory, $"{fileName}-{options.InputMarkdownType}-{options.OutputMarkdownType}.md"), converted.Result);
+
+            return 0;
         }
     }
 }
