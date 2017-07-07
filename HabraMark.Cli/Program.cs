b80dfdf26c375c9dab8dbe69037d5f86b1c9ec89
Ivan Kochurkin <kvanttt@gmail.com>
2017-07-07 22:09:21 +0300

    Images map support in Cli.

--- HabraMark.Cli/Program.cs
+++ HabraMark.Cli/Program.cs
@@ -1,4 +1,5 @@
 ﻿using CommandLine;
+using System;
 using System.IO;
 
 namespace HabraMark.Cli
@@ -19,7 +20,7 @@ namespace HabraMark.Cli
             string directory = Path.GetDirectoryName(parameters.InputFileName);
             string fileName = Path.GetFileNameWithoutExtension(parameters.InputFileName);
 
-            var data = File.ReadAllText(parameters.InputFileName);
+            string data = File.ReadAllText(parameters.InputFileName);
             var options = ProcessorOptions.GetDefaultOptions(parameters.InputMarkdownType, parameters.OutputMarkdownType);
 
             if (parameters.LinesMaxLength.HasValue)
@@ -34,11 +35,15 @@ namespace HabraMark.Cli
             if (parameters.RemoveUnwantedBreaks.HasValue)
                 options.RemoveUnwantedBreaks = parameters.RemoveUnwantedBreaks.Value;
 
-            var processor = new Processor(options) { Logger = new ConsoleLogger() };
+            var logger = new ConsoleLogger();
+            options.ImagesMap = ImagesMap.Load(parameters.ImagesMapFileName, options.CheckLinks, logger, directory);
+            options.RootDirectory = directory;
+
+            var processor = new Processor(options) { Logger = logger };
             var converted = processor.ProcessAndGetTableOfContents(data);
             //string tableOfContents = string.Join("\n", converted.TableOfContents);
 
-            File.WriteAllText(Path.Combine(directory, $"{fileName}-{options.InputMarkdownType}-{options.OutputMarkdownType}.md"), converted.Result);
+            File.WriteAllText(Path.Combine(directory, $"{fileName}-{options.InputMarkdownType}->{options.OutputMarkdownType}.md"), converted.Result);
 
             return 0;
         }
