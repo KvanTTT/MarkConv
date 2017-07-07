7d1250ee76a0d1e4d9825d9d7b9011f20bc8579d
Ivan Kochurkin <kvanttt@gmail.com>
2017-07-07 18:21:41 +0300

    Use Encoding.UTF8 as default encoding in ConsoleLogger.

--- HabraMark.Cli/ConsoleLogger.cs
+++ HabraMark.Cli/ConsoleLogger.cs
@@ -1,9 +1,15 @@
 ﻿using System;
+using System.Text;
 
 namespace HabraMark
 {
     public class ConsoleLogger : ILogger
     {
+        public ConsoleLogger()
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+        }
+
         public void Info(string message)
         {
             Console.WriteLine($"[INFO] {message}");
