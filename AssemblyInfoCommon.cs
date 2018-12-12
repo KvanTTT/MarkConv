611bc5b6672013d516219779eb30cf2259740a19
Ivan Kochurkin <kvanttt@gmail.com>
2018-12-13 00:14:16 +0300

    Add AssemblyInfoCommon.cs

---
+++ AssemblyInfoCommon.cs
@@ -0,0 +1,7 @@
+using System;
+using System.Reflection;
+
+[assembly: AssemblyProduct("MarkConv")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyVersion("1.0.0.0")]
