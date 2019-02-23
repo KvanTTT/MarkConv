a150c8bc82a326e20ced363467cc710eeb7debe1
Ivan Kochurkin <kvanttt@gmail.com>
2019-02-23 17:17:23 +0300

    Adds `readonly` modifier to all regexes, split DetailsTagRegex to DetailsOpenTagRegex and DetailsCloseTagRegex

--- MarkConv/MarkdownRegex.cs
+++ MarkConv/MarkdownRegex.cs
@@ -6,24 +6,26 @@ namespace MarkConv
     {
         private static string space = @"[ \t]";
 
-        public static string[] LineBreaks = new string[] { "\n", "\r\n" };
-        public static char[] SpaceChars = new char[] { ' ', '\t' };
+        public static readonly string[] LineBreaks = { "\n", "\r\n" };
+        public static readonly char[] SpaceChars = { ' ', '\t' };
 
-        public static Regex SpecialCharsRegex = new Regex($@"^(>|\*|-|\+|\d+\.|\||=)$", RegexOptions.Compiled);
-        public static Regex SpecialItemRegex = new Regex($@"^{space}*(>|\|)", RegexOptions.Compiled);
-        public static Regex ListItemRegex = new Regex($@"^{space}*(\*|-|\+|\d+\.){space}(.+)", RegexOptions.Compiled);
-        public static Regex CodeSectionRegex = new Regex($@"^{space}*(~~~|```)", RegexOptions.Compiled | RegexOptions.Multiline);
-        public static Regex HeaderRegex = new Regex($@"^{space}*(#+){space}*(.+)", RegexOptions.Compiled);
-        public static Regex HeaderLineRegex = new Regex($@"^{space}*(-+|=+){space}*$", RegexOptions.Compiled);
+        public static readonly Regex SpecialCharsRegex = new Regex($@"^(>|\*|-|\+|\d+\.|\||=)$", RegexOptions.Compiled);
+        public static readonly Regex SpecialItemRegex = new Regex($@"^{space}*(>|\|)", RegexOptions.Compiled);
+        public static readonly Regex ListItemRegex = new Regex($@"^{space}*(\*|-|\+|\d+\.){space}(.+)", RegexOptions.Compiled);
+        public static readonly Regex CodeSectionRegex = new Regex($@"{space}*(~~~|```)", RegexOptions.Compiled | RegexOptions.Multiline);
+        public static readonly Regex HeaderRegex = new Regex($@"^{space}*(#+){space}*(.+)", RegexOptions.Compiled);
+        public static readonly Regex HeaderLineRegex = new Regex($@"^{space}*(-+|=+){space}*$", RegexOptions.Compiled);
+        public static readonly Regex CommentRegex = new Regex(@"<!--(.|\n|\r)*?-->", RegexOptions.Compiled);
 
-        public static Regex DetailsTagRegex = new Regex(@"<\s*(/)?details\s*>", RegexOptions.Compiled);
-        public static Regex SummaryTagsRegex = new Regex(@"<\s*summary\s*>(.*?)<\s*/summary\s*>", RegexOptions.Compiled);
-        public static Regex SpoilerOpenTagRegex = new Regex(@"<\s*spoiler\s*title\s*=\s*""(.*?)""\s*>", RegexOptions.Compiled);
-        public static Regex SpoilerCloseTagRegex = new Regex(@"<\s*/spoiler\s*>", RegexOptions.Compiled);
-        public static Regex AnchorTagRegex = new Regex(@"<\s*anchor\s*>(.*?)<\s*/anchor\s*>", RegexOptions.Compiled);
-        public static Regex UrlRegex = new Regex(@"^http(s)?://", RegexOptions.Compiled);
-        public static Regex SrcUrlRegex = new Regex(@"src\s*=\s*([^\s]+)", RegexOptions.Compiled);
-        public static Regex LinkRegex = new Regex(
+        public static readonly Regex DetailsOpenTagRegex = new Regex(@"<\s*details\s*>", RegexOptions.Compiled);
+        public static readonly Regex DetailsCloseTagRegex = new Regex(@"<\s*/details\s*>", RegexOptions.Compiled);
+        public static readonly Regex SummaryTagsRegex = new Regex(@"<\s*summary\s*>(.*?)<\s*/summary\s*>", RegexOptions.Compiled);
+        public static readonly Regex SpoilerOpenTagRegex = new Regex(@"<\s*spoiler\s*title\s*=\s*""(.*?)""\s*>", RegexOptions.Compiled);
+        public static readonly Regex SpoilerCloseTagRegex = new Regex(@"<\s*/spoiler\s*>", RegexOptions.Compiled);
+        public static readonly Regex AnchorTagRegex = new Regex(@"<\s*anchor\s*>(.*?)<\s*/anchor\s*>", RegexOptions.Compiled);
+        public static readonly Regex UrlRegex = new Regex(@"^https?://", RegexOptions.Compiled);
+        public static readonly Regex SrcUrlRegex = new Regex(@"src\s*=\s*([^\s]+)", RegexOptions.Compiled);
+        public static readonly Regex LinkRegex = new Regex(
             @"(!?)" +
             @"\[(([^\[\]]|\\\])+)\]" +
             @"\(([^\)]+)\)", RegexOptions.Compiled);
