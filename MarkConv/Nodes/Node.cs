d24a29eaceb5d114f1c0b295201b08e5d5da5ee8
Ivan Kochurkin <kvanttt@gmail.com>
2020-11-09 21:53:17 +0300

    Add Start, Length properties to Node

--- MarkConv/Nodes/Node.cs
+++ MarkConv/Nodes/Node.cs
@@ -3,5 +3,15 @@
     public abstract class Node
     {
         public Node Parent { get; set; }
+
+        public int Start { get; }
+
+        public int Length { get; }
+
+        public Node(int start, int length)
+        {
+            Start = start;
+            Length = length;
+        }
     }
 }
