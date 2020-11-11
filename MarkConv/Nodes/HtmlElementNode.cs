4f9205705fc3661a0722a6c2099feb19da58b203
Ivan Kochurkin <kvanttt@gmail.com>
2020-11-11 16:32:01 +0300

    Add TryGetChild method to HtmlElementNode

--- MarkConv/Nodes/HtmlElementNode.cs
+++ MarkConv/Nodes/HtmlElementNode.cs
@@ -12,6 +12,21 @@ namespace MarkConv.Nodes
 
         public HtmlStringNode SelfClosingTag { get; }
 
+        public bool TryGetChild(string key, out Node child)
+        {
+            foreach (Node node in Content)
+            {
+                if (node is HtmlElementNode childHtmlElementNode && childHtmlElementNode.Name.String == key)
+                {
+                    child = node;
+                    return true;
+                }
+            }
+
+            child = null;
+            return false;
+        }
+
         public HtmlElementNode(HtmlParser.ElementContext elementContext, HtmlStringNode name, Dictionary<string, HtmlAttributeNode> attributes, List<Node> content,
             HtmlStringNode selfClosingTag)
             : base(elementContext)
